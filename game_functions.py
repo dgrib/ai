f96020d50b45121fc068a73e20b335949d557ec2
Dmitrii Gribanov <gds1984@bk.ru>
2020-04-11 02:14:12 +0500

    game_function refactoring

--- game_functions.py
+++ game_functions.py
@@ -2,25 +2,33 @@ import sys
 
 import pygame
 
+def check_keydown_events(event, ship):
+    """Rects keydowns"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+        """Здесь возможно использовать блоки elif , потому что каждое событие связано
+        только с одной клавишей. Если же игрок нажимает обе клавиши одновременно,
+        то программа обнаруживает два разных события"""
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Reacts keyups"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Operates key push and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-                """Здесь возможно использовать блоки elif , потому что каждое событие связано 
-                только с одной клавишей. Если же игрок нажимает обе клавиши одновременно,
-                то программа обнаруживает два разных события"""
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-
+            check_keyup_events(event, ship)
+            
 def update_screen(ai_settings, screen, ship):
 
     screen.fill(ai_settings.bg_color)
