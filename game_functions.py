b8790e875e66c3dee4f782e7c500d847017c1eb5
Dmitrii Gribanov <gds1984@bk.ru>
2020-04-11 07:45:46 +0500

    Ship up-down

--- game_functions.py
+++ game_functions.py
@@ -4,22 +4,22 @@ from bullet import Bullet
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Rects keydowns"""
-    if event.key == pygame.K_RIGHT:
-        ship.moving_right = True
+    if event.key == pygame.K_UP:
+        ship.moving_up = True
         """Здесь возможно использовать блоки elif , потому что каждое событие связано
         только с одной клавишей. Если же игрок нажимает обе клавиши одновременно,
         то программа обнаруживает два разных события"""
-    elif event.key == pygame.K_LEFT:
-        ship.moving_left = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """Reacts keyups"""
-    if event.key == pygame.K_RIGHT:
-        ship.moving_right = False
-    elif event.key == pygame.K_LEFT:
-        ship.moving_left = False
+    if event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
 
 def check_events(ai_settings, screen, ship, bullets):
     """Operates key push and mouse events"""
--- ship.py
+++ ship.py
@@ -14,13 +14,13 @@ class Ship():
 		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
 		"""Every new ship emerges beside the screen lower edge"""
-		self.rect.centerx = self.screen_rect.centerx
-		self.rect.bottom = self.screen_rect.bottom
+		self.rect.left = self.screen_rect.left
+		self.rect.centery = self.screen_rect.centery
 		# Сохранение вещественной координаты центра корабля.
-		self.center = float(self.rect.centerx)
+		self.center = float(self.rect.centery)
 		#Movoing flag
-		self.moving_right = False
-		self.moving_left = False
+		self.moving_up = False
+		self.moving_down = False
 
 	def update(self):
 		"""Renews ship position taking into account the flag"""
@@ -28,12 +28,12 @@ class Ship():
 		чтобы при нажатии обеих клавиш со стрелками атрибут rect.centerx сначала увеличивался,
 		а потом уменьшался. В результате корабль остается на месте."""
 		# Обновляется атрибут center, не rect.
-		if self.moving_right and self.rect.right < self.screen_rect.right:
-			self.center += self.ai_settings.ship_speed_factor
-		if self.moving_left and self.rect.left > 0:
+		if self.moving_up and self.rect.top > self.screen_rect.top:
 			self.center -= self.ai_settings.ship_speed_factor
+		if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+			self.center += self.ai_settings.ship_speed_factor
 		# Обновление атрибута rect на основании self.center.
-		self.rect.centerx = self.center
+		self.rect.centery = self.center
 
 	def blitme(self):
 		"""Draws a ship into the current position"""
